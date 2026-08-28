9db97d8bfaf6e729b3b1c2db8dd4a0806a9b0fd7
TheSilentNights <yangsichen080320@outlook.com>
2026-08-28 10:44:51 +0800

    fix router

--- service/server/router.go
+++ service/server/router.go
@@ -48,6 +48,17 @@ func NewRouter(
 }
 
 func (router *Router) RegisterRoutes(engine *gin.Engine) {
+	engine.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
 	engine.GET("/status", router.getStatus)
 
 	api := engine.Group("/api/v1/")
